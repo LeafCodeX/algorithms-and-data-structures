7ceda35a0b45bb512708176cf5871822a32ba2a7
Marcin Bajkowski <129612167+LeafCodeX@users.noreply.github.com>
2024-10-24 23:30:02 +0200

    Add files via upload

---
+++ how-do-I-get-there/consts.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "myString.h"
+
+using namespace std;
+
+typedef struct Vertex {
+    int vertex;
+    int weight = 1;
+} Vertex;
+
+typedef struct {
+    String name;
+    int first = -1;
+    int last = -1;
+    int vertex = -1; 
+    bool visited = false; 
+} City;
